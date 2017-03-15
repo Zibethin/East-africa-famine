fde31a5f50d9b6b52fe08849a717f037d8fcaf5b
Zibethin <nguyenththuong@gmail.com>
2017-03-15 15:59:26 +0000

    Added another line for malnourished/menu top
    changed object declaration of country list to an object, added new
    container for foodNeed number, added visibility toggle for when number =
    0, added top menu, removed menu in intro, added opacity to map
    background to make it more readable

--- js/main.js
+++ js/main.js
@@ -1,78 +1,28 @@
 //Dataset for map, chapter names, numbers and zoom 
 //Note the country code needs to be the ISO3 code
 
-var mapLocations = {
-    'africa': {
-        'camera': {
-            'duration': '4000',
-            'center': [-28.200195, 5.441022],
-            'zoom': '2.5',
-            'pitch': '0'
-        },
-        'inNeed': 70
-    },
+//colours for active and inactive countries
+var colorNotActive = 'rgba(153, 0, 0, 0.7)';
+var colorActive = 'rgba(204, 0, 0, 1)';
+var listOfISO3 = ['SSD', 'SOM', 'NGA', 'YEM', 'KEN', 'ETH'];
+var listCountryNames = ['South Sudan', 'Somalia', 'Nigeria', 'Yemen', 'Kenya', 'Ethiopia'];
 
-    'SSD': {
-        'camera': {
-            'duration': '4000',
-            'center': [1.472168, 7.231699],
-            'zoom': '3.5',
-            'pitch': '45'
-        },
-        'inNeed': 7.5
-    },
+// the following variables need to be in the same order as the country list above
+var inNeed = [7.5, 6.2, 8.5, 18.8, 2.7, 9.7];
+var foodNeed = [4.9, 2.9, 8.1, 14.1, 2.2, 0];       //ETH number not known yet
+var idp = [1.8, 1.1, 1.9, 2.0, 0, 0 ];              //ETH not known - IDP = internally displaced people
+var pitch = [45, 15, 35, 5, 50, 20];
 
-    'SOM': {
-        'camera': {
-            'duration': '4000',
-            'center': [13.908691, 5.441022],
-            'zoom': '3.5',
-            'pitch': '15'
-        },
-        'inNeed': 6.2
-    },
-
-    'NGA': {
-        'camera': {
-            'duration': '4000',
-            'center': [-20.544434, 14.434680],
-            'zoom': '3.5',
-            'pitch': '35'
-        },
-        'inNeed': 8.5
-    },
-
-    'YEM': {
-        'camera': {
-            'duration': '4000',
-            'center': [18.830566, 15.114553],
-            'zoom': '3.5',
-            'pitch': '5'
-        },
-        'inNeed': 18.8
-    },
-
-    'KEN': {
-        'camera': {
-            'duration': '4000',
-            'center': [19.808350, 6.315299],
-            'zoom': '3.5',
-            'pitch': '50'
-        },
-        'inNeed': 2.7
-    },
-    'ETH': {
-        'camera': {
-            'duration': '4000',
-            'center': [27.059326, 8.037473],
-            'zoom': '3.5',
-            'pitch': '20'
-        },
-        'inNeed': 9.7
-    },
-};
+var center = [
+    [1.472168, 7.231699],     //SSD
+    [13.908691, 5.441022],    //SOM
+    [-20.544434, 14.434680],  //NGA
+    [18.830566, 15.114553],   //YEM
+    [19.808350, 6.315299],    //KEN
+    [18.676758, 12.039321]];   //ETH
 
 //creating the layer object for map layers
+
 var LayerObject = {
     //initialising the object
     init: function (id, type, source, layout, paint, sourceLayer, filter) {
@@ -86,9 +36,53 @@ var LayerObject = {
     }
 };
 
-//color for active and inactive countries
-var colorNotActive = 'rgba(153, 0, 0, 0.7)';
-var colorActive = 'rgba(204, 0, 0, 1)';
+//creating the layer object for country camera parameters and overall numbers
+
+var CountryObject = {
+    //initialising the object
+    init: function (camera, inNeed, foodNeed, idp) {
+        this.camera = camera;
+        this.inNeed = inNeed;
+        this.foodNeed = foodNeed;
+        this.idp = idp;
+    }
+};
+
+var CameraObject = {
+    //camera 
+    init: function (duration, center, zoom, pitch) {
+        this.duration = duration;
+        this.center = center;
+        this.zoom = zoom;
+        this.pitch = pitch;
+    }
+};
+
+//initialising the locations list
+
+var mapLocations = {
+    'africa': {
+        'camera': {
+            'duration': '4000',
+            'center': [-28.200195, 5.441022],
+            'zoom': '2.5',
+            'pitch': '0'
+        },
+        'inNeed': 70,
+        'foodNeed': 0,
+        'idp': 0
+    }
+};
+
+//adding other countries to the locations list 
+
+for (var i = 0; i < listOfISO3.length; i++) {
+    var tempCamera = Object.create(CameraObject);
+    var temp = Object.create(CountryObject);
+    tempCamera.init(4000, center[i], 3.5, pitch[i]);
+    temp.init(tempCamera, inNeed[i], foodNeed[i], idp[i]);
+    mapLocations[listOfISO3[i]] = temp;
+};
 
 //creating the layers for borders and name highlighting
 
@@ -96,20 +90,13 @@ var countryBorders = Object.create(LayerObject);
 countryBorders.init('countryLine', 'line', 'countries', { 'visibility': 'visible' }, {'line-color': '#cccccc','line-width': 3}, 'ne_10m_admin_0_countries-99cdmu', ['in', 'ISO_A3']);
 
 var countryNameHighlight = Object.create(LayerObject);
-countryNameHighlight.init('countryNames', 'symbol', 'countryNames', {
-    'visibility': 'visible',
-    'text-field': '{name_en}',
-    'text-size': 14
-}, { 'text-color': '#000000' }, 'country_label', ['in', 'name_en', '']);
-console.log(countryNameHighlight);
+countryNameHighlight.init('countryNames', 'symbol', 'countryNames', { 'visibility': 'visible', 'text-field': '{name_en}', 'text-size': 14 }, { 'text-color': '#000000' },
+    'country_label', ['in', 'name_en', '']);
 
 //adding list of countries to parameters of the layer objects
 
-var listOfISO3 = ['SSD', 'SOM', 'NGA', 'KEN', 'ETH', 'YEM'];
-var listCountryNames = ['South Sudan', 'Somalia', 'Nigeria', 'Kenya', 'Ethiopia', 'Yemen'];
 countryBorders.filter.push.apply(countryBorders.filter, listOfISO3);
 countryNameHighlight.filter.push.apply(countryNameHighlight.filter, listCountryNames);
-
 
 // Setting up the background map (grey basic map labelled east-africa-famine2 on mapbox)
 
@@ -118,9 +105,12 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/brcmaps/cj00w54pm00na2ro400gflm5j',
     center: [-28.200195, 5.441022],
-    zoom: 2.5
+    zoom: 2.5,
+    interactive: false
 });
 
+
+// ------------------------------ When the map loads add all the layers --------------------------------
 
 map.on('load', function () {
 
@@ -155,7 +145,6 @@ map.on('load', function () {
     map.addLayer(countryNameHighlight);
     map.scrollZoom.disable();
 
-
     // Figuring out what chapter is on the screen
 
     window.onscroll = function () {
@@ -169,13 +158,11 @@ map.on('load', function () {
         }
     };
 
-
     function isElementOnScreen(id) {
         var element = document.getElementById(id);
         var bounds = element.getBoundingClientRect();
         return bounds.top < window.innerHeight && bounds.bottom > 0;
     }
-
 
     // setting the "active" label to chapter on screen
 
@@ -202,29 +189,50 @@ map.on('load', function () {
 
         activeChapterName = chapterName;
 
-        // fade out previous number and then fade in new number of in number of people in Need
-        html = '<div class=\'count\'> ' + mapLocations[chapterName].inNeed + '</div><div>&nbsp; million people in need</div>';
-        $('.number-container').fadeOut(1000, function () {
-            setTimeout(function () {
-                $('.number-container').html(html).fadeIn(1000, function () {
-                });
-                $('.count').each(function () {
-                    $(this).prop('Counter', 0).animate({
-                        Counter: $(this).text()
-                    }, {
-                        duration: 4000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now * 10) / 10);
-                        }
-                    });
-                });
-            }, 500)
-        })
+ 
+ 
+
+
+                // fade out previous number and then fade in new number of in number of people in Need
+                var needHtml = '<div class=\'count\'> ' + mapLocations[chapterName].inNeed + '</div><div>&nbsp; million people in need</div>';
+                var foodHtml = '<div class=\'count\'> ' + mapLocations[chapterName].foodNeed + '</div><div>&nbsp; million malnourished</div>';
+                $('.number-container').fadeOut(1000, function () {
+                    setTimeout(function () {
+
+                        // if the number of people in need is not null then add the numbers to the map
+                        if (mapLocations[chapterName].inNeed > 0) {
+                            $('#in-need').html(needHtml).fadeIn(1000, function () {
+                            });
+                        };
+
+                        if (mapLocations[chapterName].foodNeed > 0) {
+                            $('#food-need').css("visibility", "visible");
+                            $('#food-need').html(foodHtml).fadeIn(1000, function () {
+                            });
+                        };
+                        if (mapLocations[chapterName].foodNeed == 0)
+                        {
+                            $('#food-need').css("visibility", "hidden");
+                        };
+
+                        // function to animate the numbers to count up
+                        $('.count').each(function () {
+                            $(this).prop('Counter', 0).animate({
+                                Counter: $(this).text()
+                            }, {
+                                duration: 2000,
+                                easing: 'swing',
+                                step: function (now) {
+                                    $(this).text(Math.ceil(now * 10) / 10);
+                                }
+                            });
+                        });
+
+                    }, 500)
+                }) // end fadedout of number container
+
+
         oldChapter = chapterName;
     }
 
-});
-
-
-
+}); // END function on map load
